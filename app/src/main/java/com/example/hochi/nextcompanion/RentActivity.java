3f6a5811c5a9b4b475b4aed162683cac4fca9646
Fabi12345678910 <mysterytyp@gmail.com>
2022-11-04 14:55:44 +0100

    commie

--- app/src/main/java/com/example/hochi/nextcompanion/RentActivity.java
+++ app/src/main/java/com/example/hochi/nextcompanion/RentActivity.java
@@ -1,18 +1,27 @@
 package com.example.hochi.nextcompanion;
 
+import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.zxing.qrcode.detector.Detector;
+
 import org.w3c.dom.Text;
 
 public class RentActivity extends AppCompatActivity implements AsyncTaskCallbacks<String> {
+    private static final int PERMISSION_REQUEST_CAMERA = 0;
     private RequestHandler rentRequestTask = null;
+
+    private boolean cameraEnabled = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +44,32 @@ public class RentActivity extends AppCompatActivity implements AsyncTaskCallback
         }
     }
 
+    void hasCameraAccess(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            cameraEnabled = true;
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                ActivityCompat.requestPermissions(RentActivity.this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST_CAMERA);
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == PERMISSION_REQUEST_CAMERA) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                cameraEnabled = true;
+            } else {
+                com.google.zxing.qrcode.decoder.Decoder decoder;
+
+                //Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    boolean requestCameraAccess(){
+        return false;
+    }
     void rentRequest() {
         //Prepare request to rent bike
         TextView mBikeInput;
